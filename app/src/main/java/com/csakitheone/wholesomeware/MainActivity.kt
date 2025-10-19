f496776b2377435a7d33561ed77499d8b56a7952
Csáki <jockahun@gmail.com>
2025-10-19 02:55:16 +0200

    MainActivity changes

--- app/src/main/java/com/csakitheone/wholesomeware/MainActivity.kt
+++ app/src/main/java/com/csakitheone/wholesomeware/MainActivity.kt
@@ -4,17 +4,24 @@ import android.app.WallpaperManager
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
+import android.service.wallpaper.WallpaperService
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -37,9 +44,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.csakitheone.wholesomeware.wallpaper.KoloraFesztAnalogClockWallpaperService
 import com.csakitheone.wholesomeware_brand.WholesomeWare
@@ -54,14 +63,11 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen().setKeepOnScreenCondition { isKeepingSplash }
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             MainScreen()
         }
-        Timer().schedule(timerTask {
-            isKeepingSplash = false
-        }, 3000L)
     }
 
     @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
@@ -77,17 +83,28 @@ class MainActivity : ComponentActivity() {
                     Surface(
                         shadowElevation = 8.dp,
                     ) {
-                        Image(
-                            modifier = Modifier.fillMaxWidth(),
-                            painter = painterResource(id = R.drawable.wholesomeware_banner),
-                            contentDescription = null,
-                        )
+                        Box(
+                            contentAlignment = Alignment.TopCenter,
+                        ) {
+                            Image(
+                                modifier = Modifier.fillMaxWidth(),
+                                painter = painterResource(id = R.drawable.wholesomeware_banner),
+                                contentDescription = null,
+                            )
+                            Box(
+                                modifier = Modifier
+                                    .zIndex(1f)
+                                    .fillMaxWidth()
+                                    .statusBarsPadding()
+                                    .background(MaterialTheme.colorScheme.primary),
+                            )
+                        }
                     }
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(16.dp),
-                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
                     ) {
                         Text(
                             text = "An app for everything WholesomeWare related.",
@@ -97,36 +114,27 @@ class MainActivity : ComponentActivity() {
                             text = "Live wallpapers",
                             style = MaterialTheme.typography.titleMedium,
                         )
-                        Card {
-                            DropdownMenuItem(
-                                leadingIcon = {
-                                    Icon(
-                                        painter = painterResource(id = R.drawable.ic_access_time),
-                                        contentDescription = null,
+                        Card(
+                            onClick = {
+                                setLiveWallpaper(
+                                    ComponentName(
+                                        this@MainActivity,
+                                        KoloraFesztAnalogClockWallpaperService::class.java
                                     )
-                                },
-                                text = { Text("Kolora Feszt analog clock") },
-                                onClick = {
-                                    try {
-                                        val intent =
-                                            Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
-                                        intent.putExtra(
-                                            WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
-                                            ComponentName(
-                                                this@MainActivity,
-                                                KoloraFesztAnalogClockWallpaperService::class.java
-                                            )
-                                        )
-                                        startActivity(intent)
-                                    } catch (e: Exception) {
-                                        Toast.makeText(
-                                            this@MainActivity,
-                                            "Error setting wallpaper: ${e.message}",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-                                    }
-                                }
-                            )
+                                )
+                            },
+                        ) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth().padding(24.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_access_time),
+                                    contentDescription = null,
+                                )
+                                Text("Kolora Feszt analog clock")
+                            }
                         }
                         HorizontalDivider()
                         Text(
@@ -137,6 +145,24 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
+        }
+    }
+
+    fun setLiveWallpaper(wallpaperComponentName: ComponentName) {
+        try {
+            val intent =
+                Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
+            intent.putExtra(
+                WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                wallpaperComponentName
+            )
+            startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(
+                this@MainActivity,
+                "Error setting wallpaper: ${e.message}",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
